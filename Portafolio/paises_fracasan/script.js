3647411800b0e510e75c61fe70fd3cf7cb7a15c6
JesusGarciaSantiago <jesusgs0729@gmail.com>
2024-10-05 17:10:52 -0600

    paises fracasan

---
+++ Portafolio/paises_fracasan/script.js
@@ -0,0 +1,14 @@
+function loadContent(page) {
+    fetch(page)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('content').innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error loading content:', error);
+        });
+}
+
+function goBack() {
+    window.history.back();
+}
